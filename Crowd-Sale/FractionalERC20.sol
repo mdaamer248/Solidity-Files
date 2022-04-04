6874117c335711353447e8681da95e0cd57f265c
md aamer <mdaamer248@gmail.com>
2022-04-04 14:55:07 +0530

    solProd V 1

---
+++ Crowd-Sale/FractionalERC20.sol
@@ -0,0 +1,14 @@
+
+
+pragma solidity ^0.4.8;
+
+import "zeppelin/contracts/token/ERC20.sol";
+
+/**
+ * A token that defines fractional units as decimals.
+ */
+contract FractionalERC20 is ERC20 {
+
+  uint public decimals;
+
+}
